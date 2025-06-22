1e335ee3983839b1e09978219285f122b144416a
Thomas Craggs <Thomas.Craggs@city.ac.uk>
2025-06-23 00:58:10 +0100

    Update to v1.0.1

--- Formula/tidytask.rb
+++ Formula/tidytask.rb
@@ -1,8 +1,8 @@
 class Tidytask < Formula
   desc "A simple CLI tool for managing your to-do list"
   homepage "https://github.com/tm-craggs/tidytask"
-  url "https://github.com/tm-craggs/tidytask/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "7ff3097de10785a96b1b3240f048969fe7c2ae1ca63c8de1f3325af97a2e00c8"
+  url "https://github.com/tm-craggs/tidytask/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "a8b05215ecc98c2f0b368f882fc059ae80c7207468dbe329e317db0d5097d712"
   license "GPL-3.0"
 
   depends_on "go" => :build
